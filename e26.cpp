4b37f8f47a610256618eb40384c4a6d5ecb2cd25
Scott Gillespie <scottg@indeed.com>
2019-10-13 17:18:17 -0500

    truck

--- e26.cpp
+++ e26.cpp
@@ -5,22 +5,12 @@
 
 using namespace std; 
 
-// This function returns repeating sequence of 
-// a fraction. If repeating sequence doesn't 
-// exits, then returns empty string 
 string to_decimal(int n, int d) {
 	string res; // Initialize result 
 
-	// Create a map to store already seen remainders 
-	// remainder is used as key and its position in 
-	// result is stored as value. Note that we need 
-	// position for cases like 1/6. In this case, 
-	// the recurring sequence doesn't start from first 
-	// remainder. 
 	map <int, int> m; 
 	m.clear(); 
 
-	// Find first remainder 
 	int rem = n % d; 
 
 	// Keep finding remainder until either remainder 
