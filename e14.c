539d46143cad01ea4be06465cab85ea3da11e308
slg74 <scottghome@scottgs-MBP.lan>
2024-09-11 14:38:54 -0500

    cleanup

--- e14.c
+++ e14.c
@@ -30,28 +30,6 @@ int main(int argc, char *argv[]) {
 }
 
 
-/*int main() 
-{
-    long final = 0;
-    long tmp = 0;
-    long len = 0; 
-
-    long i; 
-
-    
-    for (i = 2; i < 1000001; i++) {
-        len = get_collatz_chain(i); 
-        if (len > tmp) { 
-            tmp = len; 
-            final = i;
-        }
-    }
-    printf("%ld\n", final); 
-    return 0; 
-}
-*/
-
-
 long get_collatz_chain(long n) {
 
     long count_even = 1; 
